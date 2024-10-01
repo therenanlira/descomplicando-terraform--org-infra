26da9780f15ea249a286b2f63219fa1fda7780da
Renan Lira <renanlira@bluems.com>
2024-10-01 20:37:10 -0300

    chore: refactor dev and prod locals.tf files

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,14 @@
+locals {
+  org_name    = "therenanlira"
+  environment = "dev"
+  region      = "us-east-2"
+
+  azs_count        = 1
+  vpc_cidr         = "10.0.0.0"
+  enable_ipv6      = false
+  enable_flow_logs = false
+}
+
 module "org_vpc" {
   source = "git@github.com:descomplicando-terraform/therenanlira--descomplicando-terraform--vpc-module.git"
 
--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,3 +1,14 @@
+locals {
+  org_name    = "therenanlira"
+  environment = "prod"
+  region      = "us-east-1"
+
+  azs_count        = 3
+  vpc_cidr         = "10.1.0.0"
+  enable_ipv6      = true
+  enable_flow_logs = true
+}
+
 module "org_vpc" {
   source = "git@github.com:descomplicando-terraform/therenanlira--descomplicando-terraform--vpc-module.git"
 
