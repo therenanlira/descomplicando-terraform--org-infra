5bf81426c5fac6e775fa4c3992dac35f6852c93d
Renan <22295448+therenanlira@users.noreply.github.com>
2024-10-02 01:17:01 -0300

    refactor: Update backend.tf for dev and prod environments

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "s3--descomplicando-terraform-therenanlira"
-    key    = "org/infra/terraform.tfstate"
+    key    = "dev/infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
